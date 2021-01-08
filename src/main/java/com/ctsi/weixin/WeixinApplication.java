1a1ab586d38c836cf45dda38718c3a54a9af7720
wuyufei123 <51846450+wuyufei123@users.noreply.github.com>
2021-01-08 11:09:53 +0800

    启动类修改

--- src/main/java/com/ctsi/weixin/WeixinApplication.java
+++ src/main/java/com/ctsi/weixin/WeixinApplication.java
@@ -1,9 +1,16 @@
 package com.ctsi.weixin;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.ctsi.*"})
+@MapperScan(basePackages = "com.ctsi", annotationClass = Mapper.class)
+@EnableScheduling
+@Configuration
 public class WeixinApplication {
 
     public static void main(String[] args) {
