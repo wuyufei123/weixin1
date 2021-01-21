703b83cc653c8f564c388e3ef4822cb53485f2a7
wuyufei123 <51846450+wuyufei123@users.noreply.github.com>
2021-01-21 11:25:56 +0800

    修改报名

--- src/main/java/com/tencent/WeixinApplication.java
+++ src/main/java/com/tencent/WeixinApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages={"com.tencent.*"})
-@MapperScan(basePackages = "com.tencent", annotationClass = Mapper.class)
+@SpringBootApplication(scanBasePackages = {"com.tencent.*"})
+@MapperScan(basePackages = "com.tencent.*", annotationClass = Mapper.class)
 @EnableScheduling
 @Configuration
 public class WeixinApplication {
