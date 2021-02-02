cc6cda7167621253fcb56bd24ea77936f878f489
wuyufei123 <51846450+wuyufei123@users.noreply.github.com>
2021-02-02 18:12:20 +0800

    前端功能修改1

---
+++ src/main/java/com/tencent/cron/ReactWebAppConfig.java
@@ -0,0 +1,14 @@
+package com.tencent.cron;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class ReactWebAppConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }
+}
